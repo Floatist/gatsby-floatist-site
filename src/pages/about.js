5083d771b8c165c986fa8fe860eafbe9a80fdb5c
Cindy Allis <cindyallis@floatist.com>
2022-02-20 13:39:16 +0200

    Finished gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About us">
+            <p>This site is about us</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
