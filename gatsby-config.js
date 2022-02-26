ba8f2fcc8d4c7f1d92bae6cef25703b1741f2c66
Cindy Allis <cindyallis@floatist.com>
2022-02-26 17:18:43 +0200

    Render MDX content from blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,17 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-      "gatsby-plugin-image",
-      "gatsby-plugin-sharp",
+      "gatsby-plugin-image", // render images
+      "gatsby-plugin-sharp", // render images
       {
-        resolve: "gatsby-source-filesystem",
+        resolve: "gatsby-source-filesystem", // query data using graphql
         options: {
           name: `blog`,
           path: `${__dirname}/blog`,
         }
       },
+      "gatsby-plugin-mdx", // render mdx content
+      "gatsby-remark-images", // For responsive images within mdx content
+      "gatsby-remark-autolink-headers" // To automatically create links in mdx content
     ]
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,48 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Container } from 'react-bootstrap'
 
 const BlogPage = ({ data }) => {
 
-    console.log(data.allFile.edges)
+    console.log(data.allMdx.nodes)
 
     return (
         <Layout pageTitle="Blog">
-            <p>Blog</p>
-            <p>This will be our blog</p>
-            <ul className="list-unstyled">
-
-            {
-                    data.allFile.edges.map(({ node })  => (
-                        <li key={node.name}>{node.name}</li>
+            <Container>
+                {
+                    data.allMdx.nodes.map((node) => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
 
                     ))
                 }
-            </ul>
+            </Container>
         </Layout>
     )
 }
 
 export const query = graphql`
 query {
-    allFile {
-      edges {
-        node {
-          name
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          author
+          date(formatString: "MMM DD, Y")
+          title
         }
+        id
+        body
       }
     }
   }
+  
 `
 
 export default BlogPage
